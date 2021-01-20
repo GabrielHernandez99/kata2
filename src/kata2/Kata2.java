dd93617b396f9acda5ca75aa576cec8043696001
gabri <gabri@DESKTOP-8KSQUVO>
2021-01-20 00:14:35 +0000

    Kata 2

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,17 @@
+
+package kata2;
+
+/**
+ *
+ * @author gabri
+ */
+public class Kata2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
